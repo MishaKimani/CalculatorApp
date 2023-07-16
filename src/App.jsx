d0025021c4ab78b0d6937fc1333039226f4ef065
MishaKimani <mishanyawira@gmail.com>
2023-07-16 18:31:31 +0300

    Progress

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ function App() {
 
   const [ num, setNum ] =useState(0)
   const [ num2, setNum2 ] =useState(0)
-  const [ operator, setOperator] =useState('')
+  const [ operator, setOperator] =useState()
+
 
   const handleNum = (e)=>{
     const selectedNum= e.target.innerText
@@ -18,14 +19,14 @@ function App() {
     }
   }
   
-  const add=(e)=>{
-    const selectedNum= e.target.innerText
-
-    const sum= num + selectedNum
-
-    setNum(sum)
-
+  const clear = ()=>{
+    setNum(0)
   }
+
+  const percentage = ()=>{
+    setNum(num/100)
+  }
+  
 
   return (
     <div className="w-full h-screen bg-gray-200 flex items-center justify-center">
@@ -38,9 +39,9 @@ function App() {
 
         <div className="w-full mt-5 flex flex-wrap items-center justify-center">
           
-          <div className="m-2 w-14 h-14 text-white text-2xl flex items-center justify-center bg-blue-400 rounded-full">C</div>
+          <div onClick= {clear} className="m-2 w-14 h-14 text-white text-2xl flex items-center justify-center bg-blue-400 rounded-full">AC</div>
           <div className="m-2 w-14 h-14 text-white text-2xl flex items-center justify-center bg-blue-400 rounded-full">+/-</div>
-          <div className="m-2 w-14 h-14 text-white text-2xl flex items-center justify-center bg-blue-400 rounded-full">%</div>
+          <div onClick= {percentage} className="m-2 w-14 h-14 text-white text-2xl flex items-center justify-center bg-blue-400 rounded-full">%</div>
           <div className="m-2 w-14 h-14 text-white text-2xl flex items-center justify-center bg-yellow-600 rounded-full">/</div>
           <div onClick= {handleNum} className="m-2 w-14 h-14 text-white text-2xl flex items-center justify-center bg-gray-600 rounded-full">7</div>
           <div onClick= {handleNum} className="m-2 w-14 h-14 text-white text-2xl flex items-center justify-center bg-gray-600 rounded-full">8</div>
@@ -53,7 +54,7 @@ function App() {
           <div onClick= {handleNum} className="m-2 w-14 h-14 text-white text-2xl flex items-center justify-center bg-gray-600 rounded-full">1</div>
           <div onClick= {handleNum} className="m-2 w-14 h-14 text-white text-2xl flex items-center justify-center bg-gray-600 rounded-full">2</div>
           <div onClick= {handleNum} className="m-2 w-14 h-14 text-white text-2xl flex items-center justify-center bg-gray-600 rounded-full">3</div>
-          <div onClick= {add} className="m-2 w-14 h-14 text-white text-2xl flex items-center justify-center bg-yellow-600 rounded-full">+</div>
+          <div className="m-2 w-14 h-14 text-white text-2xl flex items-center justify-center bg-yellow-600 rounded-full">+</div>
           <div onClick= {handleNum} className="m-2 w-32 h-14 text-white text-2xl flex items-center pl-7 bg-gray-600 rounded-full">0</div>
           <div className="m-2 w-14 h-14 text-white text-2xl flex items-center justify-center bg-gray-600 rounded-full">.</div>
           <div className="m-2 w-14 h-14 text-white text-2xl flex items-center justify-center bg-yellow-600 rounded-full">=</div>
